a736500e01d0cc25092f961caa08c36d7354d466
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-03-20 10:50:07 +0800

    Refactor password check program to use functions

--- prac_02/password_star.py
+++ prac_02/password_star.py
@@ -1,13 +1,42 @@
-password = input("Password: ")
-length_of_password = len(password)
-while length_of_password > 0:
-    if length_of_password > 6:
-        for i in range(length_of_password):
-            print('*', end='')
-        break
-    else:
-        print("Invalid input ")
+# password = input("Password: ")
+# length_of_password = len(password)
+# while password != '':
+#     if length_of_password > 6:
+#         for i in range(length_of_password):
+#             print('*', end='')
+#         break
+#     else:
+#         print("Try again")
+#         password = input("Password: ")
+#         length_of_password = len(password)
+
+
+def main():
+    password = get_valid_password()
+    length_of_password = len(password)
+    while password != '':
+        if length_of_password > 6:
+            return print_asterisk(length_of_password)
+        else:
+            print("Try again")
+            password = get_valid_password()
+            length_of_password = len(password)
+
+
+def print_asterisk(length_of_password):
+    for i in range(length_of_password):
+        print('*', end='')
+    return
+
+
+def get_valid_password():
+    password = input("Password: ")
+    while password == '':
+        print("Invalid input")
         password = input("Password: ")
-        length_of_password = len(password)
+    return password
 
 
+
+
+main()
