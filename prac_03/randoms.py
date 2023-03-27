71285103429f5c8f3ab2b7d93ad1d3feb50cb151
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-03-27 11:09:35 +0800

    Add prac 3 files

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+# What did you see on line 1?
+# Smallest: 5, largest: 20
+
+# What did you see on line 2?
+# Smallest: 3, Largest: 10
+# No, line 2 cannot produce a 4.
+
+# What did you see on line 3?
+# Smallest: 2.5, largest: 5.5
+
+print(random.randint(1, 100))
