c161f6e485647753c4dbfb85a91ee850daa8d092
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-04-10 14:28:35 +0800

    Add feature to store email and names and print name and email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,6 +11,13 @@ while email != '':
     name = ' '.join(emails[0].split('.')).title()
     answer = input(f"Is your name {name}? (Y/n) ")
     while answer != '' and answer != 'y':
-        name = input("Name: ")
-        answer = 'y'
+        if answer == 'n' or answer == 'no':
+            name = input("Name: ").title()
+            answer = 'y'
+        else:
+            print("Invalid input")
+            answer = input(f"Is your name {name}? (Y/n) ")
+    name_to_email[name] = email
     email = input("Email: ")
+for name, email in name_to_email.items():
+    print(f"{name} ({email})")
