8bb181438f185456644cc844142e0059bc8c27be
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-04-10 11:45:35 +0800

    Sort and format output to look nicer

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,13 +1,15 @@
 """
 Word Occurrences
 Estimate: 25 minutes
-Actual:
+Actual: 52 minutes
 """
 word_to_count = {}
 
 text = input("Text: ")
 texts = text.split()
+texts.sort()
 for i in range(len(texts)):
     word_to_count[texts[i]] = texts.count(texts[i])
+max_length = max(len(text) for text in texts)
 for word, count in word_to_count.items():
-    print(f"{word} : {count}")
+    print(f"{word:{max_length}} : {count}")
