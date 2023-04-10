da4748030c577b3ac798aafe1f812c83dc11b5e4
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-04-10 10:36:08 +0800

    Add dictionary of colour names

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+colours_to_code = {"absolute zero": "#0048ba", "cadet grey": "#91a3b0", "frostbite": "#e936a7", "goldenrod": "#daa520",
+                   "isabelline": "#f4f0ec", "magenta": "#ff00ff", "pale silver": "#c9c0bb", "purple": "#a020f0",
+                   "radical red": "#ff355e", "slate blue": "#6a5acd"}
