af6d1c0d74e8e4a0d37bf97a247c1776227e2d05
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-04-24 10:38:21 +0800

    Add __str__ and altered __init__ to accept name values

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,14 +1,13 @@
-"""CP1404/CP5632 Practical - Car class example."""
-
 
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name, fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
@@ -29,3 +28,6 @@ class Car:
             self.fuel -= distance
         self._odometer += distance
         return distance
+
+    def __str__(self):
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
