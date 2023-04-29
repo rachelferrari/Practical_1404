6958d33f05a76942a95319ba3319eea11f08edd0
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-04-29 10:13:25 +0800

    Change input file directory

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -47,7 +47,7 @@ def using_csv():
     """Language file reader version using the csv module."""
     # First, open the file for reading - note: specify newline
     # to avoid quoted \n in strings being considered a new record
-    in_file = open('../languages.csv', 'r', newline='')
+    in_file = open('languages.csv', 'r', newline='')
     in_file.readline()
     reader = csv.reader(in_file)  # use default dialect, Excel
     for row in reader:
@@ -60,7 +60,7 @@ using_csv()
 
 def using_namedtuple():
     """Language file reader version using a named tuple."""
-    in_file = open('../languages.csv', 'r', newline='')
+    in_file = open('languages.csv', 'r', newline='')
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
@@ -80,7 +80,7 @@ using_namedtuple()
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
     Language = namedtuple('Language', 'name, typing, reflection, year, pointerarithmetic')
-    in_file = open("../languages.csv", "r")
+    in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):
         print(language.name, 'was released in', language.year)
