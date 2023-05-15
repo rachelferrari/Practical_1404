8ab5ea1ffe0c923fdd9a22feca0aa1246031d716
rachelferrari <rachel.ferrari@my.jcu.edu.au>
2023-05-15 11:11:11 +0800

    Modify output of the fare into a proper format

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,4 +4,4 @@ silver_taxi = SilverServiceTaxi("Hummer", 200, 4)
 print(silver_taxi)
 another_silver_taxi = SilverServiceTaxi("Silver Taxi", 200, 2)
 another_silver_taxi.drive(18)  # 18km trip in a Silver Service Taxi
-print(another_silver_taxi.get_fare())
+print(f"${another_silver_taxi.get_fare():.2f}")
