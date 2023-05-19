d6f4a7ada6028476655ad7e0a393994557ca99b4
rachelferrari <128335613+rachelferrari@users.noreply.github.com>
2023-05-19 12:28:22 +0800

    Update dynamic_labels.py

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -5,16 +5,16 @@ from kivy.properties import StringProperty
 
 
 class DynamicLabelsApp(App):
-    """Kivy app to list names within a dictionary and dynamically creates a separate label for each one"""
+    """Kivy app to list names within a dictionary and dynamically creates a separate label for each one."""
     label_text = StringProperty()
 
     def __init__(self, **kwargs):
-        """Initialize data"""
+        """Initialize data."""
         super().__init__(**kwargs)
         self.name_to_label = {"Sinclair": "Boy", "Demian": "Friend", "Kromer": "Bully"}
 
     def build(self):
-        """Build the Kivy GUI"""
+        """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file("dynamic_labels.kv")
         for name in self.name_to_label:
