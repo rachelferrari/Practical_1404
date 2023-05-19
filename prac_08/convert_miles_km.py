0c449cc5e207d935a05354b2ced25ac77dde6cdc
rachelferrari <128335613+rachelferrari@users.noreply.github.com>
2023-05-19 12:27:33 +0800

    Update convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,28 +7,28 @@ KILOMETRES_IN_MILES = 1.60934
 
 
 class ConvertMilesToKmApp(App):
-    """ConvertMilestoKmApp is a Kivy App for converting miles (input) into kilometres (output)"""
+    """ConvertMilestoKmApp is a Kivy App for converting miles (input) into kilometres (output)."""
     result = StringProperty()
 
     def build(self):
-        """ built the Kivy app for the kv file """
+        """Built the Kivy app for the kv file."""
         Window.size = (300, 100)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculation(self):
-        """ handle calculation, output result to label widget"""
+        """Handle calculation, output result to label widget."""
         value = self.handle_invalid()
         self.result = str(value * KILOMETRES_IN_MILES)
 
     def handle_increment(self, increment):
-        """ handle increase or decrease of inputted number for calculation """
+        """Handle increase or decrease of inputted number for calculation."""
         value = self.handle_invalid()
         self.root.ids.input_number.text = str(value + increment)
 
     def handle_invalid(self):
-        """ handle invalid inputs"""
+        """Handle invalid inputs."""
         try:
             value = float(self.root.ids.input_number.text)
             return value
